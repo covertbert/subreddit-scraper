8892f9980e1d446f9f0c2803f05e0b6e9886b40b
bertie.blackman <bertie.blackman@havas.com>
2017-03-27 16:48:30 +0100

    Added missing files

---
+++ src/components/form/FormButton.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="field">
+		<div class="control">
+			<button @click="clickEvent" class="button" :class="combinedComputedClasses(modifier, isDisabledConditions)">
+				<slot></slot>
+			</button>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'FormButton',
+
+		props: {
+			clickEvent: {
+				type: Function,
+				required: true,
+			},
+			modifier: {
+				type: String,
+				required: true,
+			},
+			isDisabledConditions: {
+				type: Boolean,
+				required: true,
+			},
+		},
+
+		methods: {
+			combinedComputedClasses(modifier, isDisabledConditions) {
+				const computedClassConcat = isDisabledConditions ? 'is-disabled' : '';
+				return `${modifier} ${computedClassConcat}`;
+			},
+		},
+
+	};
+</script>
+
+<style scoped lang="scss">
+	.control,
+	button {
+		width: 100%;
+	}
+
+	.control {
+		margin-top: .5rem;
+	}
+
+	button {
+		height: 50px;
+		font-size: 1.2rem;
+	}
+</style>
